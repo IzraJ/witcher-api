8e792843cba4ba7d471d13a4495c7c880d2ac21f
IzraJ <98055348+IzraJ@users.noreply.github.com>
2022-07-03 22:03:45 -0400

    first commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-alert('working')
+document.querySelector('botton').addEventListener('click',getWitcherName)
+
+async function getWitcherName(){
+    const witchName = document.querySelector('input').value
+    try{
+        const res = await fetch()
+    }
+}

604a8918bca144e85a8f042f1ef80323885d458c
IzraJ <98055348+IzraJ@users.noreply.github.com>
2022-07-03 22:09:05 -0400

    add server main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ document.querySelector('botton').addEventListener('click',getWitcherName)
 async function getWitcherName(){
     const witchName = document.querySelector('input').value
     try{
-        const res = await fetch()
+        const res = await fetch(`https://witcher-api.herokuapp.com/api/${witchName}`)
+        const data = await res.json()
+        console.log(data)
+        document.querySelector('h2').innerText = data.name
+    } catch(err){
+        console.log(err)
     }
 }
